d2a73e2517356d644b1e5418a9f0ccd2763ffa93
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2025-03-02 17:18:07 -0500

    fd command list when run with 0 args

--- commands/fnffd/fd.py
+++ commands/fnffd/fd.py
@@ -7,6 +7,13 @@ async def run(message: discord.Message, args: list[str], client: discord.Client 
 
     if message.channel.id not in globals.fdtrusteds: return
 
+    if len(args) == 0:
+        await message.channel.send(
+"""FREECONNECT24 COMMANDS:\n
+\n
+'connect': Attempt to establish a connection with the FreeConnect24 client running on the host machine. 0 args.\n
+'disconnect': Attempt to close an already existing connection with the FreeConnect24 client running on the host machine. 0 args.""")
+        return
     command = args[0]
 
     if command == "connect":
