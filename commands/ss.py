25392bb6d50c0d72e372db31f3374267b9adf6fd
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2025-03-02 16:42:22 -0500

    removed obsolete comamnds

--- commands/ss.py
+++
@@ -1,9 +0,0 @@
-import discord
-import pyautogui
-
-async def run(message: discord.Message, args: list[str], client: discord.Client = None):
-    return await message.reply("until next time!")
-    ss = pyautogui.screenshot()
-    ss = ss.crop((1920//2-400, 1080//2-400, 1920//2+400, 1080//2+400))
-    ss.save("ss.png")
-    await message.channel.send(file=discord.File("ss.png"))
