b37799fe9e6ac8026290a15af7048f910a4e89c6
ms-kaylaa <108439098+ms-kaylaa@users.noreply.github.com>
2025-03-24 19:39:49 -0400

    randomfile story of undertale

---
+++ commands/die.py
@@ -0,0 +1,7 @@
+import discord
+import sys
+
+async def run(message: discord.Message, args: list[str], client: discord.Client = None):
+    #await message.channel.send("Self destructing")
+    #sys.exit(0)
+    pass
--- commands/files/randomfile.py
+++ commands/files/randomfile.py
@@ -17,15 +17,24 @@ def recursive_get_all_files(dir):
             file_list.append(dir + file)
 
 async def run(message: discord.Message, args: list[str], client: discord.Client = None):
-    recursive_get_all_files(FILE_DIR)
+    global file_list
+    file_list.clear()
+    if random.randint(0, 14) != 0:
+        recursive_get_all_files(FILE_DIR)
     
     
-    file = random.choice(file_list)
-    filesplit: list[str] = file.split("/")
-    owner_folder = filesplit[1]
+        file = random.choice(file_list)
+        filesplit: list[str] = file.split("/")
+        owner_folder = filesplit[1]
 
-    owner = "unknown"
-    print(owner_folder)
-    if owner_folder.isdigit():
-        owner = client.get_user(int(owner_folder)).name
-    await message.reply(f"filename: {filesplit[2]} | uploader: {owner}", file=discord.File(file))
+        owner = "unknown"
+        print(owner_folder)
+        if owner_folder.isdigit():
+            owner = client.get_user(int(owner_folder)).name
+        await message.reply(f"filename: {filesplit[2]} | uploader: {owner}", file=discord.File(file))
+    else:
+        file_list = os.listdir("C:/sou")
+
+        file = random.choice(file_list)
+
+        await message.reply(f"filename: {"aaaahhh!!"} | uploader: {random.choice(["rfisk", "cara", "toreil", "asgroe"])}", file=discord.File(f"C:/sou/{file}"))
